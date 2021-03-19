8515a653b5b4af42bed9e8c09aac60f8e5a65a3d
Vishnu Ram <vishnu.ganesan1329@gmail.com>
2021-03-19 19:08:27 +0530

    code optimisation and minor changes

--- esp32_webserver_ondemand_rtos.ino
+++ esp32_webserver_ondemand_rtos.ino
@@ -1,38 +1,59 @@
 #include <WiFiManager.h>
 
-#define TRIGGER_PIN 36
-#define PIN_RESET_BUTTON TRIGGER_PIN
 
-unsigned long currentTime;
-unsigned long previousTime = 0;
-unsigned long currentTime1;
-unsigned long previousTime1 = 0;
+#define TRIGGER_PIN 36                            // portal trigger pin 
+#define PIN_RESET_BUTTON TRIGGER_PIN              // reset trigger pin
 
+/* timeer variables */
+typedef struct
+{
+  unsigned long currentTime;
+  unsigned long previousTime;
+} Time_t;
+
+Time_t reconnect = {0, 0};
+Time_t reset = {0, 0};
+Time_t portal = {0, 0};
+
+/* Status Flag */
 bool isConfigRequest = true;
 bool isWifiConnected = false;
 
-WiFiManager wm;
+WiFiManager wm;                                   // object to the wifimanager
 
+
+
+/******************************* RTOS *********************************/
+
+/* Task Handles */
 TaskHandle_t task1_handle;
 TaskHandle_t task2_handle;
 
-SemaphoreHandle_t wifiMutex;
 
+
+
+
+/******************************* Setup ********************************/
 void setup()
 {
-
+  /* IO pin config*/
   pinMode(TRIGGER_PIN, INPUT);
+
+  /* Serial Config */
   Serial.begin(115200);
 
+  /* Wifi Config */
   WiFi.mode(WIFI_STA);
 
-  wm.setConfigPortalTimeout(60);
-  wm.setDarkMode(1);
+  wm.setConfigPortalTimeout(60);                    // portal Timeout (60 seconds)
+  wm.setDarkMode(1);                                // Dark mode
 
-  WiFi.reconnect();
+  WiFi.reconnect();                                 // wifi reconnect to connect the wifi if already configured
+
+
   Serial.println("Starting");
-
   delay(5000);
+
   if (WiFi.status() != WL_CONNECTED)
   {
     Serial.println("Wifi is not configured");
@@ -47,67 +68,89 @@ void setup()
     isWifiConnected = true;
   }
 
-  wifiMutex = xSemaphoreCreateMutex();
 
-  if (wifiMutex == pdFALSE)
-  {
-    Serial.println("Semaphore Creation Failed");
-    while (1)
-    {
-
-    }
-  }
+  /********************** RTOS config **********************/
 
   xTaskCreate(
-    WifiWatch,          /* Task function. */
-    "wifi watch task",        /* String with name of task. */
-    4096,            /* Stack size in bytes. */
-    NULL,             /* Parameter passed as input of the task */
-    1,                /* Priority of the task. */
-    &task1_handle);            /* Task handle. */
+    WifiWatch,                                /* Task function. */
+    "wifi watch task",                        /* String with name of task. */
+    4096,                                     /* Stack size in bytes. */
+    NULL,                                     /* Parameter passed as input of the task */
+    1,                                        /* Priority of the task. */
+    &task1_handle);                           /* Task handle. */
 
   xTaskCreate(
-    taskTwo,          /* Task function. */
-    "TaskTwo",        /* String with name of task. */
-    4096,            /* Stack size in bytes. */
-    NULL,             /* Parameter passed as input of the task */
-    1,                /* Priority of the task. */
-    &task2_handle);            /* Task handle. */
+    taskTwo,
+    "TaskTwo",
+    4096,
+    NULL,
+    1,
+    &task2_handle);
 
 }
 
 void loop()
 {
-  delay(1000);
+  // Idle task
 }
+
+/*
+   Task One - Wifi Watch
+   note: this task is used to monitor and change the wifi settings
+*/
 
 void WifiWatch( void * parameter )
 {
 
-
   while (1)
   {
-    /*Wifi reset sequence */
+    /* wifi reconnect */
+
+    if (WiFi.status() != WL_CONNECTED)
+    {
+      isConfigRequest = true;
+      isWifiConnected = false;
+
+      reconnect.currentTime = xTaskGetTickCount();
+      if (reconnect.currentTime - reconnect.previousTime >= 3000)
+      {
+
+        Serial.print(".");
+        WiFi.disconnect();
+        WiFi.reconnect();
+
+
+        reconnect.previousTime = reconnect.currentTime;
+      }
+    }
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      //      Serial.println("connected");
+      isConfigRequest = false;
+      isWifiConnected = true;
+    }
+
+
+    /* Wifi reset sequence */
 
     if (isConfigRequest == false && isWifiConnected == true)
     {
       if (digitalRead(PIN_RESET_BUTTON) == HIGH)
       {
-        currentTime = xTaskGetTickCount();
-        Serial.println("Press detected");
+        reset.currentTime = xTaskGetTickCount();
+        //Serial.println("Press detected");
 
-        if (currentTime - previousTime >= 1000)
+        if (reset.currentTime - reset.previousTime >= 1000)
         {
-          delay(3000);
 
+          delay(3000);
           while ( digitalRead(PIN_RESET_BUTTON) == HIGH)
           {
-            Serial.println("Long press detected");
+            //Serial.println("Long press detected");
             Serial.println("Erase settings and restart ...");
 
             delay(1000);
-            previousTime = currentTime;
-
+            reset.previousTime = reset.currentTime;
             wm.resetSettings();
 
             Serial.println("Restarting ESP");
@@ -116,6 +159,7 @@ void WifiWatch( void * parameter )
             ESP.restart();
             break;
           }
+
         }
       }
     }
@@ -125,31 +169,31 @@ void WifiWatch( void * parameter )
     if (isConfigRequest == true && isWifiConnected == false)
     {
       bool startconfigportal;
-      
+
       if ( digitalRead(TRIGGER_PIN) == HIGH)
       {
-        Serial.println("Press 1 detected");
-        
-        wm.setAPStaticIPConfig(IPAddress(10, 0, 1, 1), IPAddress(10, 0, 1, 1), IPAddress(255, 255, 255, 0));
-        
-        currentTime1 = xTaskGetTickCount();
-       
-        if (currentTime1 - previousTime1 >= 1000)
+        //        Serial.println("Press 1 detected");
+
+        wm.setAPStaticIPConfig(IPAddress(10, 0, 1, 1), IPAddress(10, 0, 1, 1), IPAddress(255, 255, 255, 0));      // Static ip for the webportal
+
+        portal.currentTime = xTaskGetTickCount();
+
+        if (portal.currentTime - portal.previousTime >= 1000)
         {
           delay(3000);
           while ( digitalRead(TRIGGER_PIN) == HIGH)
           {
             Serial.println("Starting Config portal...");
             delay(1000);
-            
-            startconfigportal = wm.startConfigPortal();
 
-            previousTime1 = currentTime1;
+            startconfigportal = wm.startConfigPortal();                                 // Starting the config portal
+
+            portal.previousTime = portal.currentTime;
             break;
           }
         }
 
-        if (!startconfigportal) 
+        if (!startconfigportal)
         {
           Serial.println("failed to connect and hit timeout");
           delay(5000);
@@ -171,31 +215,7 @@ void WifiWatch( void * parameter )
       }
 
     }
-    
-    //wifi reconnect
 
-    if (WiFi.status() != WL_CONNECTED)
-    {
-      isConfigRequest = true;
-      isWifiConnected = false;
-
-      currentTime2 = xTaskGetTickCount();
-      if (currentTime2 - previousTime2 >= 3000)
-      {
-        Serial.print(".");
-        WiFi.disconnect();
-        WiFi.reconnect();
-
-        previousTime2 = currentTime2;
-      }
-    }
-    else if (WiFi.status() == WL_CONNECTED)
-    {
-      Serial.println("connected");
-      isConfigRequest = false;
-      isWifiConnected = true;
-      delay(1000);
-    }
 
     delay(500);
   }
